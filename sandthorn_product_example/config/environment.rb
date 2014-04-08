c3d5099bd23e496aee54cd4fe23a1a899ce0b222
Morgan Hallgren <morgan.hallgren@upptec.se>
2014-04-08 12:05:38 +0200

    setup refactor

--- sandthorn_product_example/config/environment.rb
+++ sandthorn_product_example/config/environment.rb
@@ -4,9 +4,14 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 SandthornProductExample::Application.initialize!
 
-url = "sqlite://db/development.sqlite3"
+if Rails.env.development?
+  url = "sqlite://db/development.sqlite3"
+else
+  url = ENV["DATABASE_URL"]
+end
 
-#catch_all_config = [ {url: url } ]
+ap url
+
 catch_all_config = [ { driver: SandthornDriverSequel.driver_from_url(url: url) } ]
 Sandthorn.configuration = catch_all_config
 SandthornDriverSequel.migrate_db url: url
